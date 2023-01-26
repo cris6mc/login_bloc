343a2368ac8cd23d05a82fd063b268d0e434bbee
Cristian Machado <cris6mc@gmail.com>
2023-01-26 13:56:07 -0500

    proyecto inicial

---
+++ lib/login/models/username.dart
@@ -0,0 +1,16 @@
+import 'package:formz/formz.dart';
+
+enum UsernameValidationError { empty }
+
+class Username extends FormzInput<String, UsernameValidationError> {
+  const Username.pure() : super.pure('');
+  const Username.dirty([super.value = '']) : super.dirty();
+
+  @override
+  UsernameValidationError? validator(String value) {
+    if (value.isEmpty) return UsernameValidationError.empty;
+    return null;
+  }
+}
+
+//For simplicity, we are just validating the username to ensure that it is not empty but in practice you can enforce special character usage, length, etc...
